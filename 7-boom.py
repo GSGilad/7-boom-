03e67b0451551b2bdb90b21b71d6a9ce7084ea92
Gilad <75690788+GSGilad@users.noreply.github.com>
2021-01-09 19:37:58 +0200

    Delete 7-boom.py

--- 7-boom.py
+++
@@ -1,9 +0,0 @@
-set_number = int(input("Set a number : "))
-for boom in range(1, set_number+1):
-    if boom % 7 == 0 :
-        print("boom")
-        continue
-    elif '7' in str(boom) :
-        print("boom")
-    else:    
-        print(boom)
